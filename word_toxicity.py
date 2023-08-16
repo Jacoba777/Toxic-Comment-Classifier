15c1a9939aa63c6043979f3d89e3b15890ae7b3e
Jacoba777 <jacobraustin98@gmail.com>
2023-08-16 14:36:35 -0400

    temp branch for testing word toxicity estimation

--- word_toxicity.py
+++ word_toxicity.py
@@ -2,7 +2,8 @@ from typing import Dict, Set, List, Tuple
 
 
 def main():
-    smooth = 10
+    smooth = 1
+    min_freq = 3
 
     with open('dictionaries/counts.txt') as f:
         counts_txt = f.read().split('\n')
@@ -19,18 +20,19 @@ def main():
         toxic_freqs_txt = f.read().split('\n')
     toxic_freqs: Dict[str, int] = {c[0]: int(c[1]) for c in [line.split(' ') for line in toxic_freqs_txt]}
 
-    words: Dict[str, float] = dict()
-    for word in toxic_freqs:
+    toxicity: Dict[str, float] = dict()
+    for word in [w for w in toxic_freqs if (nontoxic_freqs.get(w, 0) + toxic_freqs.get(w, 0)) > min_freq]:
         word_freq_toxic = (toxic_freqs.get(word, 0) + smooth) / toxic_count_smooth
         word_freq_nontoxic = (nontoxic_freqs.get(word, 0) + smooth) / nontoxic_count_smooth
         word_toxicity = word_freq_toxic / word_freq_nontoxic
-        words[word] = word_toxicity
+        toxicity[word] = word_toxicity
 
-    sorted_words_by_toxicity = sorted(words, key=words.get, reverse=True)
+    sorted_words_by_toxicity = sorted(toxicity, key=toxicity.get, reverse=True)
+    sorted_words_by_toxicity = [w for w in sorted_words_by_toxicity if toxicity[w] > 1]
 
     with open('toxicity_results.txt', 'w', encoding='UTF-8') as f:
         for word in sorted_words_by_toxicity:
-            f.write(f'{word} {words[word]:.4f}\n')
+            f.write(f'{word} {toxicity[word]:.4f}\n')
 
 
 if __name__ == '__main__':
